3aecec8e9395ecb62de681cffabe088b5c5fda41
komatuna <tibikoro7@hotmail.co.jp>
2021-09-14 17:26:18 +0900

    practice-vue

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    devServer: {
+        port: 8000,
+    
+        // disableHostCheck: true,
+        // proxy: {
+        //     '/api/': {
+        //         target: 'http://localhost:8080'
+        //     }
+        // }
+    }
+};
